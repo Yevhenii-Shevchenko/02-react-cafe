406d58743a40536afe1bdb66ee6cf9f7543e91c9
S.Yevhenii.Serhiiovych <s.yevhenii.shevchenko@gmail.com>
2026-03-14 23:55:24 +0200

    fix error main.tsx

--- src/components/VoteStats/VoteStats.tsx
+++ src/components/VoteStats/VoteStats.tsx
@@ -1,6 +1,6 @@
 import css from "./VoteStats.module.css";
 
-import type { Votes } from "../../Type/votes";
+import type { Votes } from "../../types/votes";
 
 interface VoteStatsProps {
   votes: Votes;
